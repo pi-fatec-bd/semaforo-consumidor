6521f5ac46a28cd3c4c3436d324e7c1815331486
Felipe Messi <felipemessibraga@gmail.com>
2020-10-09 03:36:20 -0300

    include dropdown menu

--- src/main/resources/public/js/cadastro.js
+++ src/main/resources/public/js/cadastro.js
@@ -1,27 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('select');
+  M.FormSelect.init(elems);
+});
+
 $( "#criarUsuario" ).click(function() {
-  var dados = {
+    var listaEstados = document.getElementById('listaEstados');
+
+    var dados = {
       cpf: document.getElementById("cpf").value,
       anoNascimento : document.getElementById("anoNascimento").value,
       cidade: document.getElementById("cidade").value,
-      uf: document.getElementById("uf").value,
+      uf: listaEstados.options[listaEstados.selectedIndex].value,
       sexo: $("input:radio[name=sexo]:checked").val(),
-      senha: "des",
-      confirmarSenha: "des"
-  }
+      senha: document.getElementById("senha").value,
+      confirmarSenha: document.getElementById("confirmarSenha").value
+    }
   
-  $.post("http://localhost:4567/api/v1/PessoaFisica?" + $.param(dados), function() {
-      alert( "enviando..." );
-    })
-      .done(function() {
-        alert( "Cadastrado com sucesso!" );
-      })
-      .fail(function() {
-        alert( "Algum erro ocorreu, tente novamente mais tarde" );
-      })
-      .always(function() {
-        alert( "Você será redirecionado para a próxima página." );
-      });
-  })
+    $.post("http://localhost:4567/api/v1/PessoaFisica?" + $.param(dados), function() {
+        alert( "enviando..." );
+        })
+        .done(function() {
+          alert( "Cadastrado com sucesso!" );
+        })
+        .fail(function() {
+          alert( "Algum erro ocorreu, tente novamente mais tarde" );
+        })
+        .always(function() {
+          alert( "Você será redirecionado para a próxima página." );
+        });
+})
 
 // $( "#listarCadastros" ).click(function() {
 //     // $.get("http://localhost:4567/api/v1/PessoaFisica", function(resultado){
