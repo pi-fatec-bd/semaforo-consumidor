2e7bce3a38268dd8703974e3d914bccaa2f77e58
Felipe Messi <felipemessibraga@gmail.com>
2020-11-10 21:34:23 -0300

    Correção da integração com o backend

--- src/main/resources/public/js/login.js
+++ src/main/resources/public/js/login.js
@@ -1,32 +1,31 @@
-$(document).ready(function(){
-    $('loginForm').submit(function(e){    
+$('#loginForm').submit(function(e){    
+    e.preventDefault();
+    console.log("qualquer coisa")
+    const loginURL = "http://localhost:4567/api/v1/Login"
+    const usuarioForm = {
+        cpf: $('#cpf').val(),
+        senha: $('#senha').val(),
+    }
+    console.log(usuarioForm.cpf)
+    $.post(loginURL, usuarioForm, function(data) {
+            console.log(data)   
+        //Se o usuário não existir, volte para a tela inicial
+        if(data.cpf == 0){
+
+            window.location.href = '/index.html';
         
-        e.preventDefault();
-        const loginURL = "http://localhost:4567/api/v1/Login"
-        const usuarioForm = {
-            cpf: $('#cpf').val(),
-            senha: $('#senha').val(),
+        //Se o usuário existir, vá para a tela principal	
+        } else {
+        
+            sessionStorage.setItem("cpf", data.cpf);
+
+            window.location.href = '/tela-principal.html';            
         }
-        
-        $.get(loginURL, usuarioForm, function(data){
-                
-            //if user not exists, return to index page
-            if(data.doc_cli == 0){
-
-                window.location.href = '/index.html';
-            
-            //if user exists, go to user html page	
-            } else {
-            
-                sessionStorage.setItem("cpf", data.doc_cli);
-
-                window.location.href = '/user.html';            
-            }
-                      
-        });
-        
+                    
     });
+    
 });
+
 
 
 
